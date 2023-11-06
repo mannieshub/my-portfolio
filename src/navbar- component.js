9aa4b70ed27433e5e47a25a308714764d562259c
mannieshub <110074002+mannieshub@users.noreply.github.com>
2023-11-07 00:37:12 +0700

    remake all

--- src/navbar- component.js
+++ src/navbar- component.js
@@ -4,11 +4,11 @@ import React from 'react';
 const NavbarComponent = (props) => {
   const navStyles = {
     // รายละเอียดสไตล์ของ Navbar ที่คุณต้องการ
-    backgroundColor: "#4B4B4B",
+    backgroundColor: "#272727",
     textAlign: "center",
     boxShadow: "1px 4px 9px rgba(0, 0, 0, 0.25)",
     top: 0,
-    position: "fixed",
+    position: "fixed-top",
     width: "100%",
     height: "60px",
   };
